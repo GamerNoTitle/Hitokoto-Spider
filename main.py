31190cb1435a6ce8742f4e6347a66881ecc4e4fe
GamerNoTitle <bili33@87ouo.top>
2020-02-11 13:47:49 +0800

    Now it can grab the Hitokoto Successfully but the sorts are wrong

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import requests as r
 import json as js
-import xlsxwriter
 import csv
-# import panda as pd
 def create_csv(path):
     with open(path,"w+",newline="") as file:    # 打开文件，也相当于一个回车，避免覆盖文档
         csv_file = csv.writer(file)
@@ -16,26 +14,30 @@ def append_csv(path):
 print("请输入文件输出名：")
 path=input()    # 输出文件
 print("请输入抓取的数量，如果要抓取全部请到https://hitokoto.cn/status查看现在的一言总数并填入：")
-num=input() # 抓取数量
+num=int(input()) # 抓取数量
 create_csv(path)
-ids=[]
 sorts=""
-for i in range num:
+ids=['0']
+i=1
+for i in range(num):
     res = r.get('https://international.v1.hitokoto.cn/') # 得到服务器回应，此时回应的内容为json文件（res.text）和状态码
     data=res.json() # 将获取到的结果转为json字符串
-    for t in range i:
+    t=1
+    for t in range(i):
         if data["id"]==ids[t]:  # ID已经存在，即已经抓到过
             break
         else:
             t=t+1   # 自增
             if t==i:
                 ids.append(data["id"])
-                if data["type"]== a: sorts=("Anime")
-                if data["type"]== b: sorts=("Comic")
-                if data["type"]== c: sorts=("Game")
-                if data["type"]== d: sorts=("Novel")
-                if data["type"]== e: sorts=("Myself")
-                if data["type"]== f: sorts=("Internet")
-                if data["type"]== g: sorts=("Other")
+                if data["type"]== "a": sorts=("Anime")
+                if data["type"]== "b": sorts=("Comic")
+                if data["type"]== "c": sorts=("Game")
+                if data["type"]== "d": sorts=("Novel")
+                if data["type"]== "e": sorts=("Myself")
+                if data["type"]== "f": sorts=("Internet")
+                if data["type"]== "g": sorts=("Other")
     temp=[data["id"],sorts,data["hitokoto"]]
+    print(res.text)
     append_csv(path)
+    print("已完成数量："+str(i+1))
