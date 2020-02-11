7ff4ecc5dac3d82665afbd64f4a75d8f8a201947
GamerNoTitle <bili33@87ouo.top>
2020-02-11 09:04:32 +0800

    Updated README.md

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import requests as r
 import json as js
-res = r.get('https://v1.hitokoto.cn/') # Get Response from the Link
+res = r.get('https://international.v1.hitokoto.cn/') # Get Response from the Link
 print(res.text)
