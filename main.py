0213e8e6931ad302bb7a62a7258cfc94801101fb
GamerNoTitle <bili33@87ouo.top>
2020-02-12 17:09:01 +0800

    删除无关代码及更新README

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 import datetime
 from array import array
 import time
+print("本项目由Github@GamerNoTitle制作")
+print("------------------------------")
 # 程序运行时间开始
 start_Pro=datetime.datetime.now()
 def create_csv(path):
@@ -27,51 +29,6 @@ create_csv(path)
 sorts=""
 i=1
 temp=array('i',[0])   # 初始化temp变量，用于放置已抓取的ID
-# for i in range(num):
-#     time.sleep(delay)
-#     print("正在获取新的一言……")
-#     res = r.get('https://international.v1.hitokoto.cn/',timeout=60) # 得到服务器回应，此时回应的内容为json文件（res.text）和状态码
-#     data=res.json() # 将获取到的结果转为json字符串
-#     temp_minus=len(temp)-1
-#     if temp_minus!=0:
-#         t=1
-#         print("正在检测是否抓取过结果……")
-#         for t in range(len(temp)):
-#             if(int(data["id"])==temp[t]):
-#                 print("发现已经抓取到的结果，正在丢弃……")
-#                 i=i-1
-#                 break
-#             elif(t==len(temp)-1):
-#                 print("未抓取过的结果，正在存入文件……")
-#                 if data["type"]== "a": sorts=("Anime")  # 自动把分类码还原为分类
-#                 if data["type"]== "b": sorts=("Comic")
-#                 if data["type"]== "c": sorts=("Game")
-#                 if data["type"]== "d": sorts=("Novel")
-#                 if data["type"]== "e": sorts=("Myself")
-#                 if data["type"]== "f": sorts=("Internet")
-#                 if data["type"]== "g": sorts=("Other")
-#                 inputs=[data["id"],sorts,data["hitokoto"]]
-#                 # print(res.text)   # 输出一言，如需要把最前面的#去掉即可
-#                 append_csv(path)
-#                 temp.append(data["id"])
-#                 end_Pro=datetime.datetime.now()
-#                 print("已完成数量："+str(i+1)+'，已经用时：'+str(end_Pro-start_Pro))
-#                 break
-#     else:
-#         if data["type"]== "a": sorts=("Anime")  # 自动把分类码还原为分类
-#         if data["type"]== "b": sorts=("Comic")
-#         if data["type"]== "c": sorts=("Game")
-#         if data["type"]== "d": sorts=("Novel")
-#         if data["type"]== "e": sorts=("Myself")
-#         if data["type"]== "f": sorts=("Internet")
-#         if data["type"]== "g": sorts=("Other")
-#         inputs=[data["id"],sorts,data["hitokoto"]]
-#         # print(res.text) # 输出一言，如需要把最前面的#去掉即可
-#         append_csv(path)
-#         temp.append(data["id"])
-#         end_Pro=datetime.datetime.now()
-#         print("已完成数量："+str(i+1)+'，已经用时：'+str(end_Pro-start_Pro))
-
 while True:
     if(i==num):
         break
