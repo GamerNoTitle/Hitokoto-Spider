9d870075be11e667d12c18ae49bfd95cba36698b
GamerNoTitle <bili33@87ouo.top>
2020-03-03 09:58:47 +0800

    修复因csv表格中数量比配置数量要大而导致的抓取未停止的BUG

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ if(conf['created_at']): heads.append("created_at")
 temp=array('i',[0])   # 初始化temp变量，用于放置已抓取的ID
 if (os.path.exists(path)!=True):    # 判断文件是否存在，不存在则创建
     create_csv(path)
-    i=1
+    i=0
 else:
     print('断点续抓模式已开启！')
     file=open(path,'r',encoding='utf8')
@@ -48,13 +48,13 @@ else:
             temp.append(int(id_in_file[0])) # 将文件中已有的id加入temp数组
         except ValueError:
             id_in_file[0] = 0   # 读取已有文件时"id"无法被识别为int型所以要去掉
-        i=1
+        i=0
         i=i+len(temp)
 sorts=""
 dup=0
 all=0   # 总抓取次数
 while True:
-    if(i==num+1):   # 如果不加1那么最后一次将无法运行
+    if(i>=num):   # 如果不加1那么最后一次将无法运行
         break
     time.sleep(delay)
     print("----------------------------------------------------------")
@@ -128,7 +128,7 @@ while True:
                 append_csv(path)
                 temp.append(data["id"])
                 end_Pro=datetime.datetime.now()
-                print("已完成数量：{}/{}，已经用时：{} ，总抓取{}次，重复次数{}次，重复率{}".format(i,num,end_Pro-start_Pro,all,dup,dup/all))
+                print("已完成数量：{}/{}，已经用时：{} ，总抓取{}次，重复次数{}次，重复率{}".format(i+1,num,end_Pro-start_Pro,all,dup,dup/all))
                 i=i+1
                 break
     else:
@@ -184,7 +184,7 @@ while True:
         append_csv(path)
         temp.append(data["id"])
         end_Pro=datetime.datetime.now()
-        print("已完成数量：{}/{}，已经用时：{} ，总抓取{}次，重复次数{}次，重复率{}".format(i,num,end_Pro-start_Pro,all,dup,dup/all))
+        print("已完成数量：{}/{}，已经用时：{} ，总抓取{}次，重复次数{}次，重复率{}".format(i+1,num,end_Pro-start_Pro,all,dup,dup/all))
         i=i+1
 end_Pro=datetime.datetime.now()
 print('----------------------------------------------------------')
